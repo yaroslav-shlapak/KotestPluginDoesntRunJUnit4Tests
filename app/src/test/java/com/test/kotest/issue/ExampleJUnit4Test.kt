520fc6410cedac8978cf611f4288e4695ea4565c
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-03-22 13:50:07 +0200

    initial commit

---
+++ app/src/test/java/com/test/kotest/issue/ExampleJUnit4Test.kt
@@ -0,0 +1,12 @@
+package com.test.kotest.issue
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleJUnit4Test {
+    @Test
+    fun `addition is correct`() {
+        assertEquals(4, 2 + 2)
+    }
+}
---
+++ app/src/test/java/com/test/kotest/issue/KotestTest.kt
@@ -0,0 +1,11 @@
+package com.test.kotest.issue
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class KotestTest : FreeSpec({
+
+    "addition is correct"- {
+        2 + 2 shouldBe 4
+    }
+})
